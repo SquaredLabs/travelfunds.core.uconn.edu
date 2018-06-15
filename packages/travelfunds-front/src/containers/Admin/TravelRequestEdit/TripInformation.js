9dc85d3d7769d27455a213ef36a74072af7ec1e2
Brandon Cheng <gluxon@users.noreply.github.com>
2018-06-15 19:00:47 -0400

    Extend <TripInformation> and improve appearance

--- packages/travelfunds-front/src/containers/Admin/TravelRequestEdit/TripInformation.js
+++ packages/travelfunds-front/src/containers/Admin/TravelRequestEdit/TripInformation.js
@@ -1,12 +1,40 @@
 import React from 'react'
+import Paper from '@material-ui/core/Paper'
+import Divider from '@material-ui/core/Divider'
+import { format } from 'date-fns'
+import styles from './styles.scss'
 
 export default ({ trip }) =>
-  <div>
+  <Paper className={styles.tripInformation}>
     <h1>Travel Request {trip.id}</h1>
+    <Divider />
+    <h2>Traveler</h2>
     <dl>
-      <dt>Name</dt><dd>{trip.firstName} {trip.lastName}</dd>
-      <dt>NetID</dt><dd>{trip.netid}</dd>
-      <dt>Department</dt><dd>{trip.department}</dd>
-      <dt>Title</dt><dd>{trip.title}</dd>
+      <dt>Name</dt>
+      <dd>{trip.firstName} {trip.lastName}</dd>
+
+      <dt>NetID</dt>
+      <dd>{trip.netid}</dd>
+
+      <dt>Department</dt>
+      <dd>{trip.department}</dd>
+
+      <dt>Title</dt>
+      <dd>{trip.title}</dd>
+
+      <dt>Terminal Degree Year</dt>
+      <dd>{trip.yearOfTerminalDegree}{trip.isForSenior ? ' (senior)' : ''}</dd>
     </dl>
-  </div>
+    <h2>Trip</h2>
+    <dl>
+      <dt>Period</dt>
+      <dd>
+        {format(trip.duration[0], 'MMMM Do, YYYY')} –
+        {format(trip.duration[1], 'MMMM Do, YYYY')}
+      </dd>
+      <dt>Destination</dt><dd>{trip.destination}</dd>
+      <dt>Event Title</dt><dd>{trip.eventTitle}</dd>
+      <dt>Participation Level</dt><dd>{trip.participationLevel}</dd>
+      <dt>Method of Travel</dt><dd>{trip.primaryMethodOfTravel}</dd>
+    </dl>
+  </Paper>
