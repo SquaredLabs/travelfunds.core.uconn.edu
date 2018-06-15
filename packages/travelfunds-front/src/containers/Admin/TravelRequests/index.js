1b13dcca2dba6bb9a5847be64ec632f836e33697
Brandon Cheng <gluxon@users.noreply.github.com>
2018-06-15 17:47:32 -0400

    Add pagination and sorting to travel requests

--- packages/travelfunds-front/src/containers/Admin/TravelRequests/index.js
+++ packages/travelfunds-front/src/containers/Admin/TravelRequests/index.js
@@ -5,35 +5,55 @@ import fetch from 'isomorphic-fetch'
 import { format } from 'date-fns'
 import { Link } from 'react-router-dom'
 
-import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import Table from '@material-ui/core/Table'
 import TableBody from '@material-ui/core/TableBody'
 import TableCell from '@material-ui/core/TableCell'
 import TableHead from '@material-ui/core/TableHead'
 import TableRow from '@material-ui/core/TableRow'
+import TablePagination from '@material-ui/core/TablePagination'
+import TableSortLabel from '@material-ui/core/TableSortLabel'
 import Icon from '@material-ui/core/Icon'
 import IconButton from '@material-ui/core/IconButton'
-
-const StyledTableCell = withStyles(theme => ({
-  head: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white
-  }
-}))(TableCell)
-
-const styles = theme => ({
-  row: {
-    'transition-duration': '0.2s',
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.background.default
-    }
-  }
-})
 
 @observer
 class TravelRequests extends React.Component {
   @observable trips = []
+  @observable page = 0
+  @observable rowsPerPage = 25
+  @observable sortDirection = 'desc'
+  @observable sortColumn = 'ID'
+
+  columns = [
+    {
+      label: 'ID',
+      value: trip => trip.id,
+      getSortProperty: trip => trip.id
+    },
+    {
+      label: 'Status',
+      value: trip => trip.status,
+      getSortProperty: trip => trip.status
+    },
+    {
+      label: 'Traveler',
+      value: trip => `${trip.firstName} ${trip.lastName}`,
+      getSortProperty: trip => `${trip.firstName} ${trip.lastName}`
+    },
+    {
+      label: 'Event Start',
+      value: trip => format(trip.duration[0], 'MMM Do YYYY'),
+      getSortProperty: trip => trip.duration[0]
+    },
+    {
+      label: 'Actions',
+      value: trip =>
+        <IconButton component={props =>
+          <Link to={`/admin/trips/${trip.id}`} {...props} />}>
+          <Icon>edit</Icon>
+        </IconButton>
+    }
+  ]
 
   @action async fetchTrips () {
     const res = await fetch('/api/trips', { credentials: 'include' })
@@ -44,48 +64,88 @@ class TravelRequests extends React.Component {
     this.fetchTrips()
   }
 
-  get head () {
-    return <TableHead>
-      <TableRow>
-        <StyledTableCell>ID</StyledTableCell>
-        <StyledTableCell>Status</StyledTableCell>
-        <StyledTableCell>Traveler</StyledTableCell>
-        <StyledTableCell>Event Start</StyledTableCell>
-        <StyledTableCell>Actions</StyledTableCell>
-      </TableRow>
-    </TableHead>
+  handleSort = (ev, column) => {
+    if (this.sortColumn === column.label) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc'
+    }
+    this.sortColumn = column.label
+
+    this.trips = this.trips.sort((a, b) => {
+      const aProp = column.getSortProperty(a)
+      const bProp = column.getSortProperty(b)
+
+      if (aProp === bProp) return 0
+      if (aProp < bProp) return this.sortDirection === 'asc' ? -1 : 1
+      if (aProp > bProp) return this.sortDirection === 'asc' ? 1 : -1
+    })
   }
 
-  @computed get body () {
-    const { classes } = this.props
-    return <TableBody>
-      {this.trips.map(trip =>
-        <TableRow
-          key={trip.id}
-          className={classes.row}
-          hover>
-          <StyledTableCell>{trip.id}</StyledTableCell>
-          <StyledTableCell>{trip.status}</StyledTableCell>
-          <StyledTableCell>{`${trip.firstName} ${trip.lastName}`}</StyledTableCell>
-          <StyledTableCell>{format(trip.duration[0], 'MMM Do YYYY')}</StyledTableCell>
-          <StyledTableCell>
-            <IconButton component={props =>
-              <Link to={`/admin/trips/${trip.id}`} {...props} />}>
-              <Icon>edit</Icon>
-            </IconButton>
-          </StyledTableCell>
-        </TableRow>)}
-    </TableBody>
+  @computed get visibleTrips () {
+    return this.trips.slice(
+      this.page * this.rowsPerPage,
+      (this.page + 1) * this.rowsPerPage
+    )
   }
 
   render () {
     return <Paper>
       <Table>
-        {this.head}
-        {this.body}
+        <Head
+          sortDirection={this.sortDirection}
+          sortColumn={this.sortColumn}
+          columns={this.columns}
+          onSort={this.handleSort}
+        />
+        <Body
+          trips={this.visibleTrips}
+          columns={this.columns}
+        />
       </Table>
+      <TablePagination
+        component='div'
+        count={this.trips.length}
+        page={this.page}
+        rowsPerPage={this.rowsPerPage}
+        rowsPerPageOptions={[10, 25, 50]}
+        onChangePage={(_, page) => { this.page = page }}
+        onChangeRowsPerPage={ev => { this.rowsPerPage = ev.target.value }}
+      />
     </Paper>
   }
 }
 
-export default withStyles(styles)(TravelRequests)
+const Head = observer(({ sortDirection, sortColumn, columns, onSort }) =>
+  <TableHead >
+    <TableRow>
+      {columns.map(column =>
+        <TableCell key={column.label}>
+          {column.getSortProperty
+            ? (
+              <TableSortLabel
+                direction={sortDirection}
+                active={sortColumn === column.label}
+                onClick={ev => onSort(ev, column)}
+                children={column.label}
+              />
+            )
+            : column.label}
+        </TableCell>
+      )}
+    </TableRow>
+  </TableHead>
+)
+
+const Body = observer(({ trips, columns }) =>
+  <TableBody>
+    {trips.map(trip =>
+      <TableRow
+        key={trip.id}
+        hover>
+        {columns.map(column =>
+          <TableCell key={column.label}>{column.value(trip)}</TableCell>
+        )}
+      </TableRow>)}
+  </TableBody>
+)
+
+export default TravelRequests
