9c8a923ac2e0d3ac7d9c222eb8bb5f3855b50654
Brandon Cheng <gluxon@users.noreply.github.com>
2018-07-11 12:46:05 -0400

    Fix typo in OVPR name

--- packages/travelfunds-front/src/components/Footer/index.js
+++ packages/travelfunds-front/src/components/Footer/index.js
@@ -5,7 +5,7 @@ export default () => (
   <div className={styles.container}>
     Designed and Developed by Squared Labs (a unit of <a target='_blank' rel='noopener noreferrer' href='http://core.uconn.edu/'>COR²E</a>)
     <br />
-    <a target='_blank' rel='noopener noreferrer' href='http://research.uconn.edu'>Office of the Vice President of Research</a>
+    <a target='_blank' rel='noopener noreferrer' href='http://research.uconn.edu'>Office of the Vice President for Research</a>
     <br />
     © 2017 <a target='_blank' rel='noopener noreferrer' href='https://uconn.edu/'>University of Connecticut</a>
   </div>
