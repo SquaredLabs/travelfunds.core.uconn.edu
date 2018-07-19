723bc1ae81d97218d4fe4ca13cbfd88b0c596eb0
Brandon Cheng <gluxon@users.noreply.github.com>
2018-07-19 07:55:40 -0400

    Remove old admin.js entrypoint

--- packages/travelfunds-front/src/admin.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import GrantedFundsTable from 'components/GrantedFundsTable'
-
-const App = () =>
-  <GrantedFundsTable {...window.editRequestedFunds} />
-
-ReactDOM.render(<App />, document.getElementById('grant-costs-control'))
