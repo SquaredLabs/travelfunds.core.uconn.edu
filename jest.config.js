4ac2737d05769e80270404daec57ce9dabd4d8f5
Brandon Cheng <gluxon@users.noreply.github.com>
2019-06-02 13:44:05 -0700

    List Jest projects in a file instead of CLI args

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  projects: [
+    {
+      displayName: 'travelfunds-api',
+      rootDir: 'packages/travelfunds-api',
+      testEnvironment: 'node'
+    },
+    {
+      displayName: 'travelfunds-db',
+      rootDir: 'packages/travelfunds-db',
+      testEnvironment: 'node'
+    }
+  ]
+}
